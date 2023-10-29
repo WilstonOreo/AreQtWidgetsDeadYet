2d7c71e89727dfb0671cf3f327f02244e0ddb2c9
Michael Winkelmann <michael@winkelmann.site>
2023-10-29 21:59:47 +0100

    remove qt_logo from routes

--- server/src/main.rs
+++ server/src/main.rs
@@ -22,7 +22,6 @@ async fn main() {
 
     let app = Router::new()
         .route("/", get(index))
-        .route("/qtlogo.svg", get(qt_logo_svg))
         .route("/favicon.ico", get(favicon))
         .route("/qtloader.js", get(qt_loader_js))
         .route("/AreQtWidgetsDeadYet.html", get(index))
@@ -43,6 +42,13 @@ async fn main() {
         .unwrap();
 }
 
+macro_rules! route_get_text {
+    () => {
+        .route("/", get(index))
+
+    };
+}
+
 
 async fn index() -> impl IntoResponse {
     let mut headers = HeaderMap::new();
@@ -53,32 +59,23 @@ async fn index() -> impl IntoResponse {
     (headers, std::include_str!("../assets/index.html"))
 }
 
-
 async fn favicon() -> impl IntoResponse {
     let mut headers = HeaderMap::new();
-    headers.insert(header::CONTENT_TYPE, "image/vnd.microsoft.icon".parse().unwrap());
+    headers.insert(header::CONTENT_TYPE, "image/x-icon".parse().unwrap());
     (headers, std::include_bytes!("../assets/favicon.ico"))
 }
 
-async fn qt_logo_svg() -> impl IntoResponse {
-    let mut headers = HeaderMap::new();
-    headers.insert(header::CONTENT_TYPE, "image/svg+xml".parse().unwrap());
-    (headers, std::include_str!("../assets/qtlogo.svg"))
-}
-
-
 async fn qt_loader_js() -> impl IntoResponse {
     let mut headers = HeaderMap::new();
-    headers.insert(header::CONTENT_TYPE, "text/javascript".parse().unwrap());
+    headers.insert(header::CONTENT_TYPE, "application/javascript".parse().unwrap());
     headers.insert("Cross-Origin-Embedder-Policy", "require-corp".parse().unwrap());
     headers.insert("Cross-Origin-Opener-Policy", "same-origin".parse().unwrap());
     (headers, std::include_str!("../assets/qtloader.js"))
 }
 
-
 async fn wasm_loader_js() -> impl IntoResponse {
     let mut headers = HeaderMap::new();
-    headers.insert(header::CONTENT_TYPE, "text/javascript".parse().unwrap());
+    headers.insert(header::CONTENT_TYPE, "application/javascript".parse().unwrap());
     headers.insert("Cross-Origin-Embedder-Policy", "require-corp".parse().unwrap());
     headers.insert("Cross-Origin-Opener-Policy", "same-origin".parse().unwrap());
     (headers, std::include_str!("../assets/AreQtWidgetsDeadYet.js"))
@@ -86,7 +83,7 @@ async fn wasm_loader_js() -> impl IntoResponse {
 
 async fn wasm_worker_js() -> impl IntoResponse {
     let mut headers = HeaderMap::new();
-    headers.insert(header::CONTENT_TYPE, "text/javascript".parse().unwrap());
+    headers.insert(header::CONTENT_TYPE, "application/javascript".parse().unwrap());
     headers.insert("Cross-Origin-Embedder-Policy", "require-corp".parse().unwrap());
     headers.insert("Cross-Origin-Opener-Policy", "same-origin".parse().unwrap());
     (headers, std::include_str!("../assets/AreQtWidgetsDeadYet.worker.js"))
